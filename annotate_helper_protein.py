4178477829493fac9dcca1bd2e05030ff659170a
Your Name <il328@cornell.edu>
2026-07-14 22:40:18 -0400

    annotated api analysis

--- annotate_helper_protein.py
+++ annotate_helper_protein.py
@@ -2,9 +2,9 @@ from AnnotatorCore import setoncokbapitoken, validate_oncokb_token
 from AnnotatorCore import processalterationevents, QueryType, MutationStatus
 
 import os
-TOKEN = os.environ["ONCOKB_TOKEN"]
-INPUT_FILE = "exonic_only.txt"        
-OUTPUT_FILE = "exonic_protein_annotated.txt"
+TOKEN = "d6ed9c4e-e823-43eb-9705-b7c88c7e0eb1"
+INPUT_FILE = "hersh_exome_biallelic_annovar_annotated.hg38_multianno.requested_predictor_scores.tsv"        
+OUTPUT_FILE = "exonic_toolscores_oncokb_apicall.txt"
 
 
 setoncokbapitoken(TOKEN)
